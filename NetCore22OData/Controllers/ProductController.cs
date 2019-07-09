efb4c9be9553be9998c35f66bbd47405b9185a1f
Renicius Pagotto <renicius.pagotto@outlook.com>
2019-07-08 22:32:41 -0300

    First commit

---
+++ NetCore22OData/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using FizzWare.NBuilder;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace NetCore22OData.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = Builder<Product>.CreateListOfSize(10).Build();
+            return products;
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SmallDescription { get; set; }
+        public string Size { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ NetCore22OData/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetCore22OData
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddOData();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routeBuilder => {
+                routeBuilder.EnableDependencyInjection();
+                routeBuilder.Expand().Select().OrderBy().Filter();
+            });
+        }
+    }
+}
